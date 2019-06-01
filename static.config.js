aa7230696f57d8ea734d192bbde9db69a5a65c67
Keerati <keerati.jear@gmail.com>
2019-06-02 04:25:03 +0700

    [ADD] add config with plugins

--- static.config.js
+++ static.config.js
@@ -1,38 +1,6 @@
-import React, { Component } from 'react'
-import { ServerStyleSheet } from 'styled-components'
-
 export default {
-  getRoutes: async () => [
-    {
-      path: '/',
-      component: 'src/containers/Home'
-    },
-    {
-      is404: true,
-      component: 'src/containers/404'
-    }
+  plugins: [
+    "react-static-plugin-styled-components",
+    "react-static-plugin-react-router"
   ],
-  renderToHtml: (render, Comp, meta) => {
-    const sheet = new ServerStyleSheet()
-    const html = render(sheet.collectStyles(<Comp />))
-    meta.styleTags = sheet.getStyleElement()
-    return html
-  },Document: class CustomHtml extends Component {
-    render() {
-      const { Html, Head, Body, children, renderMeta } = this.props
-
-      return (
-        <Html>
-          <Head>
-            <meta charSet="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-            <title>{`Keerati - Personal Website`}</title>
-            {renderMeta.styleTags}
-
-          </Head>
-          <Body>{children}</Body>
-        </Html>
-      )
-    }
-  }
 }
