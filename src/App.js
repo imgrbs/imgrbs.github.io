525694c3544c3b496510d27f05b3a5a2c4b5cf54
Keerati <keerati.jear@gmail.com>
2019-06-02 04:25:13 +0700

    [ADD] add app routes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,29 @@
 import React, { Component } from 'react'
+import { Root, Routes } from 'react-static'
+import { Switch, Route } from 'react-router-dom'
+import { createGlobalStyle } from 'styled-components'
 
-import './app.css'
-import logo from './logo.png'
+import Home from './pages/Home'
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: "Kanit", sans-serif;
+  }
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <span>
-            Learn{' '}
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              React
-            </a>{' '}
-            and{' '}
-            <a
-              className="App-link"
-              href="https://react-static.js.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              React Static
-            </a>
-          </span>
-        </header>
-      </div>
+      <Root>
+        <GlobalStyles />
+        <React.Suspense fallback={'Loading...'}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route render={() => <Routes />} />
+          </Switch>
+        </React.Suspense>
+      </Root>
     )
   }
 }
